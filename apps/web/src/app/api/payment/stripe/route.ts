5a39d3a63e4e8afe21c39a5c5267265f7ef124e0
ativodou <ativodou@gmail.com>
2026-03-09 14:35:07 -0400

    fix: force-dynamic on stripe route

--- apps/web/src/app/api/payment/stripe/route.ts
+++ apps/web/src/app/api/payment/stripe/route.ts
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: NextRequest) {
