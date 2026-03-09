1905de1c7a301d66d54fc625aefe2f4ddf4e0b88
ativodou <ativodou@gmail.com>
2026-03-09 14:31:41 -0400

    fix: remove apiVersion from Stripe constructor

--- apps/web/src/app/api/payment/stripe/route.ts
+++ apps/web/src/app/api/payment/stripe/route.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, { apiVersion: "2025-01-27.acacia" });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function POST(req: NextRequest) {
   try {
